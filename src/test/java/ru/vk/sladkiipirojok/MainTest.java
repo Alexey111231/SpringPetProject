70a76317119fe68d42d0737838e2e11a37d1bc5c
Alexey <atestat9@mail.ru>
2021-01-31 15:26:11 +0300

    test CI

---
+++ src/test/java/ru/vk/sladkiipirojok/MainTest.java
@@ -0,0 +1,14 @@
+package ru.vk.sladkiipirojok;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void TestCI() {
+        System.out.println("Test");
+        System.out.println("Test");
+    }
+}
