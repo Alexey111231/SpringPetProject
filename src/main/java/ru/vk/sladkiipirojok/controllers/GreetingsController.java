9c895c19b589646aba2ab6d2f7ef66bb820ee969
Alexey <atestat9@mail.ru>
2021-02-02 23:12:22 +0300

    first application

---
+++ src/main/java/ru/vk/sladkiipirojok/controllers/GreetingsController.java
@@ -0,0 +1,21 @@
+package ru.vk.sladkiipirojok.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingsController {
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+    @GetMapping
+    public String greeting(Model model) {
+        model.addAttribute("some", "hello, raz");
+        return "main";
+    }
+}
